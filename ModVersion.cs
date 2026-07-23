17ba9712c5574f936c022479e2832a7b4bab4613
Kinetic Napier <u61819269@gmail.com>
2026-07-23 17:58:28 +0900

    Bump mod version to 0.18.0

--- ModVersion.cs
+++ ModVersion.cs
@@ -2,6 +2,6 @@ namespace Kiner.ADOFAIEditorQoL
 {
     internal static class ModVersion
     {
-        internal const string Current = "0.17.5";
+        internal const string Current = "0.18.0";
     }
 }
