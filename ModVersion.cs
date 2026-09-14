98257283ea74f6cdb705a4304270d534efb37e8e
Kinetic Napier <u61819269@gmail.com>
2026-09-15 02:08:37 +0900

    chore: bump perf test build marker

--- ModVersion.cs
+++ ModVersion.cs
@@ -2,6 +2,6 @@ namespace Kiner.ADOFAIEditorQoL
 {
     internal static class ModVersion
     {
-        internal const string Current = "0.19.3-perf-pr11-bridge";
+        internal const string Current = "0.19.3-perf-pr11-02";
     }
 }
