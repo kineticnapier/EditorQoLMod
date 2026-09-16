84074bdd07b59a0ab584e95266fae43a55520d38
Kinetic Napier <u61819269@gmail.com>
2026-09-16 19:44:08 +0900

    chore: bump asset probe version to 0.19.9

--- ModVersion.cs
+++ ModVersion.cs
@@ -2,6 +2,6 @@ namespace Kiner.ADOFAIEditorQoL
 {
     internal static class ModVersion
     {
-        internal const string Current = "0.19.8";
+        internal const string Current = "0.19.9";
     }
 }
